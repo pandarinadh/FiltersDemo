1cb3b3974a90ae5ffbe8bf5c29da13f569cc85bd
pandarinadh <39387722+pandarinadh@users.noreply.github.com>
2018-06-13 11:19:46 -0400

    Add project files.

---
+++ FiltersDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FiltersDemo.Controllers.Filters;
+
+namespace FiltersDemo.Controllers
+{
+    public class HomeController : Controller
+    {   
+        [CustomAuthorization]
+        [CustomAction]
+        [CustomResultAttribute]
+        [CustomExceptionAttribute]
+        public ActionResult Index()
+        {
+            //throw new Exception("Dummy Exception");
+            ViewBag.Message = "Index Action of Home controller is being called.";
+            return View();
+        }
+
+    }
+}
